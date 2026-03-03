814298f7d17c9c3035dba51e63e8ede438fe8584
shupermail88-hash <shupermail88@gmail.com>
2026-03-03 22:20:20 +0800

    Add files via upload

---
+++ week1assignmentprint.py
@@ -0,0 +1,29 @@
+# Question 1
+print("Hello, World!, Hello, World!\n")
+
+# Question 2
+print("Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n")
+
+# Question 3
+print("*\n**\n***\n****\n*****")
+
+# Question 4
+print(" ___________________ ")
+print("|                   |")
+print("|                   |")
+print("|    H         I    |")
+print("|                   |")
+print("|         _         |")
+print("|                   |")
+print("|___________________|\n")
+
+# Question 5
+print("Sun Mon Tue Wed Thu Fri Sat")
+print("        1   2   3   4   5")
+print("6   7   8   9   10  11  12 ")
+print("13  14  15. 16  17 18  19")
+print("20  21  22  23  24  25  26")
+print("27  28  29  30\n")
+
+# Question 6
+print("===================================\nEXAM RESULT SLIP\n===================================\nName    : Chang Boon Shu\nCourse  : Programming with Python\nMarks   : 8A\nGrade   : A\n-----------------------------------\nRemarks   : 'Exellent performance!'\n===================================")
